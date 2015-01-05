47aa94e084933c6b0fb031f488c84d5ac0c1d980
jlgoldb2 <jlgoldb2@asu.edu>
2015-01-04 19:51:29 -0700

    test for parse returning list of tuples

--- test/xmhell_test.exs
+++ test/xmhell_test.exs
@@ -1,7 +1,26 @@
 defmodule XmhellTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  def sample_xml do
+    """
+    <outtermost tag="true">
+      <first>I am first</first>
+      <div class="col-lg-6>
+        <nester>
+          <nested> info 1 </nested>
+          <nested> info 2 </nested>
+          <nested order="Last"> info 3 </nested>
+        </nester>
+      </div>
+      <third @value="12345"></third>
+    </outtermost>
+    """
+  end
+
+
+  test "sample is returned as a list of tuples" do
+    result = Xmlhell.parse(sample_xml)
+    assert is_list(result)
+    assert result |> hd |> is_tuple
   end
 end
