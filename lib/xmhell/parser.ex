3c7c41879c8fd59db12bb0764d28554866bbdd80
jlgoldb2 <jlgoldb2@asu.edu>
2015-01-06 23:04:47 -0700

    replaced String lengths and slice with a pattern match

--- lib/xmhell/parser.ex
+++ lib/xmhell/parser.ex
@@ -1,8 +1,8 @@
 defmodule Xmhell.Parser do
 
-  @opening ~r/(<([-:\w]+)(((\s+)[\w]+)*(=(""|"[-:\w]+"))*)*>)/
-  @closing ~r{<\/\s*[\w-]+\s*>}
-  @self_closing ~r/(<([-:\w]+)(((\s+)[\w]+)*(=(""|"[-:\w]+"))*)*\/>)/
+  @opening ~r/\A(<([-:\w]+)(((\s+)[\w]+)*(=(""|"[-:\w]+"))*)*>)/
+  @closing ~r{\A<\/\s*[\w-]+\s*>}
+  @self_closing ~r/\A(<([-:\w]+)(((\s+)[\w]+)*(=(""|"[-:\w]+"))*)*\/>)/
 
   def get_regexes do
     %{
@@ -12,49 +12,42 @@ defmodule Xmhell.Parser do
     }
   end
 
-  def process(xml) do
-    process(xml, [])
-  end 
+  def process(xml), do: process(xml, [])
+  def process("",  parsed), do: parsed
+  def process(xml, parsed) do
+    {entity_type, next_entity} = parse_next_entity(xml)
+    new_xml = remove_tag(xml, next_entity)
+    process(new_xml, parsed ++ [{entity_type, next_entity}])
+  end
 
-  def process(xml, list) do
+  def remove_tag(xml, next_entity) do
+    next_entity <> rest = xml
+  end
+
+  def parse_next_entity(xml) do
     xml
       |> clean_whitespace
-      |> get_opening_tag
-      |> remove_the_tag(xml)
-      |> remove_closing_tag
+      |> get_next_group
+  end
+
+  def get_next_group(xml) do
+    cond do
+      Regex.match?(@opening, xml) -> {:open, get_tag(@opening, xml) }
+      Regex.match?(@closing, xml) -> {:close, get_tag(@opening, xml) }
+      Regex.match?(@self_closing, xml) -> {:open, get_tag(@opening, xml) }
+    end
+  end
+
+  def get_tag(regex, xml) do
+    Regex.scan(regex, xml) |> hd |> hd
   end
 
   def clean_whitespace(string) do
     string |> String.rstrip |> String.lstrip
   end
 
-
-  @doc """
-  Gets opening tag of an xml string. The opening tag 
-  regex is #{inspect @opening}
-  """
-  def get_opening_tag(xml) do
-    Regex.scan(@opening, xml) |> hd |> hd
-  end
-
-  def get_closing_tag(xml) do
-    Regex.scan(@closing, xml) |> tl |> hd
-  end
-
   def remove_the_tag(tag, xml) do
     String.split_at(xml, String.length(tag))
   end
-
-  def slice_from_end_of(closing_tag, xml) do
-    new_length = String.length(xml) - String.length(closing_tag)
-    String.slice(xml, 0..new_length)
-  end
-
-  def remove_closing_tag(xml) do
-    xml
-      |> get_closing_tag
-      |> slice_from_end_of(xml)
-  end
-
 
 end
